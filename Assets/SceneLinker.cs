f701533f4c102a024cefa28989411926f6326631
Tiger <tiger.collins@outlook.com>
2021-10-14 00:23:25 +1000

    Splash Screen. Linking scenes. Buttons now auto select

---
+++ Assets/SceneLinker.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneLinker : MonoBehaviour
+{
+    internal static SceneLinker instance;
+
+
+
+    [Header("Target Menu")]
+    [SerializeField]
+    bool _checkGameStateEveryFrame = false;
+    [SerializeField]
+    TargetGameState targetGameState;
+
+    [Space(10)]
+
+    [SerializeField]
+    UnityEvent OnTargetGameStateChange; //Functions by programmers can be added by programmers, designers and others can add via inspector
+
+
+    //Hidden
+    TargetGameState previousTargetState;
+
+    public enum TargetGameState
+    {
+        normalGameplay,
+        firstTimeGameplay,
+        tutorialGameplay,
+        pauseGameplay,
+        gameOverGameplay,
+        mainMenu
+    }
+
+    public void Awake()
+    {
+        //If statement destroys this gameobject if the instance already exists, stops multiple static instances
+        
+        if (instance == null)
+        {
+            instance = this;
+
+            //This object won't destroy on sceneload
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //Adds fucntion to the OnTargetGameStateChange event - when the event is called, the function is called.
+        OnTargetGameStateChange.AddListener(delegate { TargetGameStateActivater(); });
+    }
+
+    public void TargetGameStateActivater()
+    {
+        Debug.Log("hmm");
+        switch (targetGameState)
+        {
+            case TargetGameState.normalGameplay:
+                if (SceneManager.GetActiveScene().buildIndex != 1)
+                {
+                    SceneManager.LoadSceneAsync(1);
+                }
+                break;
+            case TargetGameState.firstTimeGameplay:
+                if(SceneManager.GetActiveScene().buildIndex != 1)
+                {
+                    SceneManager.LoadSceneAsync(1);
+                }
+                
+                break;
+            case TargetGameState.tutorialGameplay:
+                if (SceneManager.GetActiveScene().buildIndex != 1)
+                {
+                    SceneManager.LoadSceneAsync(1);
+                }
+                break;
+            case TargetGameState.pauseGameplay:
+                if (SceneManager.GetActiveScene().buildIndex != 1)
+                {
+                    SceneManager.LoadSceneAsync(1);
+                }
+                break;
+            case TargetGameState.gameOverGameplay:
+                if (SceneManager.GetActiveScene().buildIndex != 1)
+                {
+                    SceneManager.LoadSceneAsync(1);
+                }
+                break;
+            case TargetGameState.mainMenu:
+                if (SceneManager.GetActiveScene().buildIndex != 0)
+                {
+                    SceneManager.LoadSceneAsync(0);
+                }
+                break;
+            default:
+                break;
+        }
+        UIHandler.instance.SetMenu();
+    }
+
+    //Runs check everyframe - poor optimisation, should be event based - UPDATE
+    private void Update()
+    {
+        //If target game state is different to the prior one (last frame), run code
+        if(targetGameState != previousTargetState && _checkGameStateEveryFrame)
+        {
+            previousTargetState = targetGameState;
+            OnTargetGameStateChange.Invoke();
+        }
+    }
+
+    protected internal TargetGameState GameStateDestination
+    {
+        get
+        {
+            return targetGameState;
+        }
+
+        set
+        {
+            targetGameState = value;
+            OnTargetGameStateChange.Invoke();
+        }
+    }
+        
+}
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
 
 public class UIHandler : MonoBehaviour
 {
@@ -14,6 +18,23 @@ public class UIHandler : MonoBehaviour
     [SerializeField]
     List<ButtonTextVisualiser> buttonTextVisualisers;
 
+    [Header("Canvas Groups")]
+    [SerializeField]
+    List<CanvasGroup> allcanvasGroups;
+    [SerializeField]
+    CanvasGroupDetails firstTimeCanvasGroup;
+     [SerializeField]
+    CanvasGroupDetails tutorialCanvasGroup;
+     [SerializeField]
+    CanvasGroupDetails pauseCanvasGroup;
+     [SerializeField]
+    CanvasGroupDetails gameOverCanvasGroup;
+     [SerializeField]
+    CanvasGroupDetails gameplayCanvasGroup;
+
+
+
+    //Called before start (game object does not need to be active)
     public void Awake()
     {
         //If statement destroys this component if the instance already exists, stops multiple static instances
@@ -25,6 +46,134 @@ public class UIHandler : MonoBehaviour
         else
         {
             Destroy(this);
+        }
+        
+    }
+
+    //Called first frame of game object active
+    private void Start()
+    {
+            SetMenu();
+    }
+
+    public void SetMenu()
+    {
+        switch (SceneLinker.instance.GameStateDestination)
+        {
+            case SceneLinker.TargetGameState.normalGameplay:
+                foreach (CanvasGroup item in allcanvasGroups)
+                {
+                    if(gameplayCanvasGroup != null)
+                    {
+                        if (item != gameplayCanvasGroup.canvasGroup)
+                        {
+                            item.interactable = false;
+                            item.blocksRaycasts = false;
+                            item.alpha = 0;
+                        }
+
+                        else
+                        {
+                            gameplayCanvasGroup.canvasGroup.alpha = 1;
+                            gameplayCanvasGroup.canvasGroup.blocksRaycasts = true;
+                            gameplayCanvasGroup.canvasGroup.interactable = true;
+                        }
+                    }
+                }
+                ChangeButtonSelection(gameplayCanvasGroup.firstButton);
+                break;
+            case SceneLinker.TargetGameState.firstTimeGameplay:
+                foreach (CanvasGroup item in allcanvasGroups)
+                {
+                    if (firstTimeCanvasGroup != null)
+                    {
+                        if (item != firstTimeCanvasGroup.canvasGroup)
+                        {
+                            item.interactable = false;
+                            item.blocksRaycasts = false;
+                            item.alpha = 0;
+                        }
+
+                        else
+                        {
+                            firstTimeCanvasGroup.canvasGroup.alpha = 1;
+                            firstTimeCanvasGroup.canvasGroup.blocksRaycasts = true;
+                            firstTimeCanvasGroup.canvasGroup.interactable = true;
+                        }
+                    }
+                }
+                ChangeButtonSelection(firstTimeCanvasGroup.firstButton);
+                break;
+            case SceneLinker.TargetGameState.tutorialGameplay:
+                foreach (CanvasGroup item in allcanvasGroups)
+                {
+                    if (tutorialCanvasGroup != null)
+                    {
+                        if (item != tutorialCanvasGroup.canvasGroup)
+                        {
+                            item.interactable = false;
+                            item.blocksRaycasts = false;
+                            item.alpha = 0;
+                        }
+
+                        else
+                        {
+                            tutorialCanvasGroup.canvasGroup.alpha = 1;
+                            tutorialCanvasGroup.canvasGroup.blocksRaycasts = true;
+                            tutorialCanvasGroup.canvasGroup.interactable = true;
+                        }
+                    }
+                }
+                ChangeButtonSelection(tutorialCanvasGroup.firstButton);
+                break;
+            case SceneLinker.TargetGameState.pauseGameplay:
+                foreach (CanvasGroup item in allcanvasGroups)
+                {
+                    if (pauseCanvasGroup != null)
+                    {
+                        if (item != pauseCanvasGroup.canvasGroup)
+                        {
+                            item.interactable = false;
+                            item.blocksRaycasts = false;
+                            item.alpha = 0;
+                        }
+
+                        else
+                        {
+                            pauseCanvasGroup.canvasGroup.alpha = 1;
+                            pauseCanvasGroup.canvasGroup.blocksRaycasts = true;
+                            pauseCanvasGroup.canvasGroup.interactable = true;
+                        }
+                    }
+                    ChangeButtonSelection(pauseCanvasGroup.firstButton);
+                }
+                break;
+            case SceneLinker.TargetGameState.gameOverGameplay:
+                foreach (CanvasGroup item in allcanvasGroups)
+                {
+                    if (gameOverCanvasGroup != null)
+                    {
+                        if (item != gameOverCanvasGroup.canvasGroup)
+                        {
+                            item.interactable = false;
+                            item.blocksRaycasts = false;
+                            item.alpha = 0;
+                        }
+
+                        else
+                        {
+                            gameOverCanvasGroup.canvasGroup.alpha = 1;
+                            gameOverCanvasGroup.canvasGroup.blocksRaycasts = true;
+                            gameOverCanvasGroup.canvasGroup.interactable = true;
+                        }
+                    }
+                }
+                ChangeButtonSelection(gameOverCanvasGroup.firstButton);
+                break;
+            case SceneLinker.TargetGameState.mainMenu:
+                break;
+            default:
+                break;
         }
         
     }
@@ -45,4 +194,46 @@ public class UIHandler : MonoBehaviour
             }    
         }
     }
+
+    public void ChangeButtonSelection(Button button)
+    {
+        if(button!=null)
+        {
+            eventSystem.SetSelectedGameObject(button.gameObject);
+        }
+
+        else
+        {
+            eventSystem.SetSelectedGameObject(null);
+        }
+       
+    }
+
+    public void StartGameFirstTime()
+    {
+        SceneLinker.instance.GameStateDestination = SceneLinker.TargetGameState.firstTimeGameplay;
+        
+    }
+
+    public void StartGameInstant()
+    {
+
+    }
+
+    //If the game is within the engine, the editor will stop playing but if the project is packaged, the application is closed
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
+
+[System.Serializable]
+public class CanvasGroupDetails
+{
+    public CanvasGroup canvasGroup;
+    public Button firstButton;
 }
